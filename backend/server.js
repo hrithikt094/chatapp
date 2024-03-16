96573412885ac52cefe4f8e076d60386ea67fa3c
hrithikt094@gmail.com <hrithikt094@gmail.com>
2024-03-17 03:59:46 +0530

    deployed first change

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes); 
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 server.listen(5000, () => {
